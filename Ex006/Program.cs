f8c01c0b9827792adc7337d38a6dfce519dc211a
rebeca <bekkacostarezende@gmail.com>
2025-01-15 21:28:14 -0300

    Atualização da aula 15/01/25

---
+++ Ex006/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2;
+            float media;
+            Console.WriteLine("Digite uma nota: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite outra nota: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            media = (num1 + num2) / 2;
+            Console.WriteLine("A média é: " + media);
+
+
+        }
+    }
+}
+    
---
+++ Ex007/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final;
+            string resultado;
+
+            Console.WriteLine("Digite nota final do aluno:");
+            nota_final = Convert.ToDouble(Console.ReadLine());
+
+            if (nota_final >= 70)
+            {
+       
+                if ( nota_final >= 95)
+                {
+                    resultado = "aprovado com louvor";
+                }
+                else
+                {
+                    resultado = "aprovado";
+                }
+                
+            }
+            else if (nota_final >=45)
+            {
+                resultado = "recuperação";
+            }
+            else
+            {
+                resultado = "reprovado";
+            }
+            Console.WriteLine("Nota do aluno: {0} - Resultado: {1}", nota_final, resultado);
+
+
+                
+        }
+    }
+}
