c4ace207d01988ca7468a69d38283244eb7d177f
rebeca <bekkacostarezende@gmail.com>
2025-01-20 19:41:42 -0300

    Exercicio do teste do triangulo

---
+++ Ex008/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final, n1;
+           string resultado;
+
+            Console.WriteLine("Digite uma velocidade: ");
+            n1= Convert.ToDouble(Console.ReadLine());  
+
+            if (nota_final >80)
+            {
+                resultado = Console.ReadLine();
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Ex009/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.Write("Digite o valor do lado A: ");
+            ladoA  = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado A: ");
+            ladoB  = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado A: ");
+            ladoC  = Convert.ToInt32(Console.ReadLine()); 
+        }
+    }
+}
