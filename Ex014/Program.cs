bf6253b775f9abf034ce24e748ae659e25fa1e9a
rebeca <bekkacostarezende@gmail.com>
2025-01-22 21:58:37 -0300

    Exercicio validação

---
+++ Ex014/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um número");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine("{0} * {1} = {2}", numero, i, numero*i);
+            }
+        }
+    }
+}
