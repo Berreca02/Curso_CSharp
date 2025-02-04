ec0639dff4a3217927c4842a85ee36795a63f004
rebeca <bekkacostarezende@gmail.com>
2025-02-03 21:19:26 -0300

    Fatorial

---
+++ Exercicio Area do Triangulo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio_Area_do_Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************");
+            Console.WriteLine("Calcular a Área do Triângulo");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+
+            Console.Write("Digite o valor da Base (m): ");
+            double B = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite o valor da Altura (m): ");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            area(B, A);
+
+        }
+
+        static void area(double Base, double Altura)
+        {
+            double a = (Base * Altura) / 2;
+            Console.WriteLine($"A área do triângulo é: {a} m²");
+
+
+        }
+
+    }
+}
